7258d443f6a6f7086753b952df97c06af3a25cf9
Yakobh <jacob.hans99@gmail.com>
2023-10-14 21:34:22 -0400

    adding persistent velocity and player rotation

--- ligma-2d-client/Assets/Scripts/PlayerController.cs
+++ ligma-2d-client/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float userSpeed = 2;
-    public float rotateSpeed = 3;
+    public float userSpeed = 1.25;
+    public float rotateSpeed = 1.5;
+    public float maxSpeed = 1.5;
     public Transform player_sprite;
     float _horizontal_input, _vertical_input;
+
+    Vector2 player_movement_vector = Vector2.zero;
     Vector2 horizontal_vector, vertical_vector, move_vector;
 
-    float diff_angle;
+    float target_angle;
 
     public static PlayerController Local;
 
@@ -27,15 +30,20 @@ public class PlayerController : MonoBehaviour
         _vertical_input = Input.GetAxis("Vertical");
         horizontal_vector = Vector2.right * _horizontal_input;
         vertical_vector = Vector2.up * _vertical_input;
-        move_vector = (horizontal_vector + vertical_vector) * userSpeed * Time.deltaTime;
-        transform.Translate(move_vector);
+        move_vector = horizontal_vector + vertical_vector;
+
+        player_movement_vector += move_vector / 500;
+        player_movement_vector = Vector2.ClampMagnitude(player_movement_vector, maxSpeed);
+
+        transform.Translate(player_movement_vector * userSpeed * Time.deltaTime);
 
         if (move_vector != Vector2.zero)
         {
-            diff_angle = Mathf.Atan2(move_vector.y - Vector2.up.y, move_vector.x - Vector2.up.x) * 180 / Mathf.PI;
-            Debug.Log(diff_angle);
-
-            player_sprite.Rotate(0.0f, 0.0f, diff_angle * rotateSpeed * Time.deltaTime);
+            target_angle = Vector2.Angle(Vector2.up, move_vector);
+            // need to figure out how tf to do these angles
+            if (_horizontal_input > 0)
+                target_angle += 180;
+            player_sprite.transform.eulerAngles = new Vector3(0.0f, 0.0f, Mathf.Lerp(player_sprite.transform.eulerAngles.z, target_angle, Time.deltaTime * rotateSpeed));
         }
     }
 }
